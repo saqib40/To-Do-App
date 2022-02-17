9d06f7950e38c4b15bbd700dfaa57db14143fd98
saqib hussain <hussainsakib44@gmail.com>
2022-02-17 16:29:50 +0530

    Add files via upload

---
+++ To-Do App/project.js
@@ -0,0 +1,27 @@
+let deleteButton = document.querySelector('.delete');
+let box = document.querySelector('.box');
+let button = document.querySelector('button');
+let textTask = document.querySelector('.textTask');
+let taskToDo = document.querySelector('.tasktodo');
+
+function deletion() {
+  
+}
+
+function check() {
+  if (box.checked === true) {
+    taskToDo.setAttribute('id', 'tasktodo');
+  }
+  else {
+    taskToDo.setAttribute('id', 'bullshit');
+  }
+}
+
+function add() {
+  
+}
+
+
+box.addEventListener('click', check);
+button.addEventListener('click', add)
+deleteButton.addEventListener('click', deletion);
